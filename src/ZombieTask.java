d5c4da28d4b7b6a21ef0c53d81486434588c9c18
Jeremias Wong <jellyjellyrobot@gmail.com>
2014-09-27 01:08:51 +0800

    updated zombietask to include storageapi

--- src/ZombieTask.java
+++ src/ZombieTask.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  * 
  * Whoa hold on there, please wait till this is up first before making
@@ -8,24 +11,38 @@
  * @author nil
  * @author jellymac
  * 
+ * @version 0.0.0a
+ * 
  */
 
 
 public class ZombieTask {
 
 	/*
-	 * Instantiation of Class Objects
+	 * Instantiation of External Objects
 	 */
 	
-	UI ui = new UI();
-	Interpreter parser = new Interpreter();
-	StorageAPI storage = new StorageAPI();
+	private static UI ui = new UI();
+	private static Interpreter parser = new Interpreter();
+	private static StorageAPI storage = new StorageAPI();
+	private static Scanner sc = new Scanner(System.in);
+	
+	/*
+	 * Instantiation of ZombieTask data attributes
+	 */
+	
+	private static String lastCommandString = null;
+	private static Command lastCommand = null;
+	private static String fileName = null;
+	private static ArrayList<Command> commandList = new ArrayList<Command>();
+	
 	
 	/*
 	 * Standard Messages
 	 */
 	
-	private static String WELCOME_MESSAGE = "Welcome to Zombie Task!";
+	private static String MESSAGE_WELCOME = "Welcome to Zombie Task!";
+	private static String MESSAGE_FILE_OPENED = "%s is ready for use";
 	
 	/**
 	 * 
@@ -35,9 +52,29 @@ public class ZombieTask {
 	 */
 	
 	public static void main(String[] args) {
-		UI.print(WELCOME_MESSAGE)
+		
+		System.out.println(WELCOME_MESSAGE);
+		initStorage(args);
+		
+		while(sc.hasNext()){
+			lastCommand = sc.nextLine();
+			
+		}
+	}
+
+	private static void initStorage(String[] args) {
+		if (args.length > 0){
+			storage.setFile(args[0]);
+		}
+		storage.createFile();
 	}
 	
+	/*
+	 * filename mutators
+	 */
 	
+	/*
+	 * commandList mutators
+	 */
 
 }
