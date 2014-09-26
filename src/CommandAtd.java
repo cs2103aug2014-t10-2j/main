0fa33404c57c419a6785e0e63730faf3d2030304
ping <ngsp1994@gmail.com>
2014-09-26 14:15:43 +0800

    Added test cases to Command Atd for invalid/missing args

--- src/CommandAtd.java
+++ src/CommandAtd.java
@@ -1,5 +1,6 @@
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -16,12 +17,18 @@ public class CommandAtd {
 	
 	@Test
 	public void testCommand() {
+		// Test 1: valid arguments
 		Command undo = new Command("undo");
 		assertEquals("undo", undo.getCommandType());
+		
+		// Test 2: invalid/missing arguments
+		undo = new Command(null);
+		assertEquals(null, undo.getCommandType());
 	}
 	
 	@Test
 	public void testCommandAdd() {
+		// Test 1: valid arguments
 		Calendar date = new GregorianCalendar(2014, 1, 1);
 		ArrayList<String> tags = new ArrayList<String>();
 		tags.add("food");
@@ -32,29 +39,58 @@ public class CommandAtd {
 		assertEquals(date, add.getDateTime());
 		assertEquals(tags, add.getTags());
 		assertFalse(add.hasMissingArgs());
+		
+		// Test 2: invalid/missing arguments
+		add = new CommandAdd("", null, null, null, true);
+		assertEquals("", add.getCommandType());
+		assertEquals(null, add.getTaskName());
+		assertEquals(null, add.getDateTime());
+		assertEquals(null, add.getTags());
+		assertTrue("", add.hasMissingArgs());
 	}
 	
 	@Test
 	public void testCommandDelete() {
+		// Test 1: valid arguments
 		CommandDelete delete = new CommandDelete("delete", 1, false);
 		assertEquals("delete", delete.getCommandType());
 		assertEquals(1, delete.getLineNo());
 		assertFalse(delete.hasMissingArgs());
+		
+		// Test 2: invalid/missing arguments
+		delete = new CommandDelete(null, -1, true);
+		assertEquals(null, delete.getCommandType());
+		assertEquals(-1, delete.getLineNo());
+		assertTrue(delete.hasMissingArgs());
 	}
 	
 	@Test
 	public void testCommandUpdate() {
+		// Test 1: valid arguments
 		CommandUpdate update = new CommandUpdate("update", 1, false);
 		assertEquals("update", update.getCommandType());
 		assertEquals(1, update.getLineNo());
 		assertFalse(update.hasMissingArgs());
+		
+		// Test 2: invalid/missing arguments
+		update = new CommandUpdate(null, -1, true);
+		assertEquals(null, update.getCommandType());
+		assertEquals(-1, update.getLineNo());
+		assertTrue(update.hasMissingArgs());
 	}
 	
 	@Test
 	public void testCommandView() {
+		// Test 1: valid arguments
 		CommandView view = new CommandView("view", "calendar", false);
 		assertEquals("view", view.getCommandType());
 		assertEquals("calendar", view.getViewType());
 		assertFalse(view.hasMissingArgs());
+		
+		// Test 2: invalid/missing arguments
+		view = new CommandView(null, "", true);
+		assertEquals(null, view.getCommandType());
+		assertEquals("", view.getViewType());
+		assertTrue(view.hasMissingArgs());
 	}
 }
