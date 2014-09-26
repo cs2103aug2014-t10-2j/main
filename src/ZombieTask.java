12259c9ea64ae520d286d8740fe85fd4372899b3
Jeremias Wong <jellyjellyrobot@gmail.com>
2014-09-26 20:37:28 +0800

    trying out EGit for team

--- src/ZombieTask.java
+++ src/ZombieTask.java
@@ -1,9 +1,35 @@
+/**
+ * 
+ * Whoa hold on there, please wait till this is up first before making
+ * any comments on how good or bad this project is.
+ * 
+ * @author ngsp94
+ * @author ljiy369
+ * @author nil
+ * @author jellymac
+ * 
+ */
+
 
 public class ZombieTask {
 
+	/**
+	 * Standard Messages
+	 */
+	
+	private static String WELCOME_MESSAGE = "Welcome to Zombie Task!";
+	
+	/**
+	 * 
+	 * Method that will be invoked when ZombieTask is called.
+	 * 
+	 * @param args
+	 */
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
+	
+	
 
 }
