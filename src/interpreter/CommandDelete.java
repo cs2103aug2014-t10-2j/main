115c6e96927b1133e1d60aa7591939e8fbf963ca
ping <ngsp1994@gmail.com>
2014-11-04 01:57:35 +0800

    updates: - changed location character to '@' - changed escape sequence to inverted commas - allowed index range for delete in 2 formats (f1, f2 and f1-f5) - allow flexible update by field - return commandAdd obj hasMissingArgs, but with arguments intact, null/empty arraylist for unspecified fields

--- src/interpreter/CommandDelete.java
+++ src/interpreter/CommandDelete.java
@@ -1,31 +1,39 @@
 package interpreter;
+
+import java.util.ArrayList;
+
 /**
- * Class for Delete command, which contains line code to identify task to
- * delete, and a boolean variable to indicate missing arguments. 
+ * Class for Delete command, which contains line codes to identify task to
+ * delete, and a boolean variable to indicate missing arguments.
+ * 
  * @author SP
  *
  */
-public class CommandDelete extends Command{
-	String lineCode = null;
-	
+public class CommandDelete extends Command {
+	ArrayList<String> lineCode = null;
+
 	/**
-	 * Constructor for the Delete command object. It is initialised with a line
-	 * number and a boolean variable to indicate if there are missing arguments.
+	 * Constructor for the Delete command object. It is initialised with a list
+	 * of line codes and a boolean variable to indicate if there are missing
+	 * arguments.
+	 * 
 	 * @param newLineCode the line code of the task to be deleted
 	 * @param userInput the original user input
 	 * @param hasMissingArgs boolean variable to indicate missing arguments
 	 */
-	public CommandDelete(String newLineCode, String userInput, boolean hasMissingArgs) {
+	public CommandDelete(ArrayList<String> newLineCode, String userInput,
+			boolean hasMissingArgs) {
 		super(Command.DELETE, userInput, hasMissingArgs);
 		lineCode = newLineCode;
 	}
-	
+
 	/**
 	 * Accessor for line code
-	 * @return the line code of the task to be deleted
+	 * 
+	 * @return a list of line codes of the tasks to be deleted
 	 */
-	public String getLineCode() {
+	public ArrayList<String> getLineCode() {
 		return lineCode;
 	}
-	
+
 }
